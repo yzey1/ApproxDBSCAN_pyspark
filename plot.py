aab9c6014d12a508a7ab373c82186ae75759c038
Yzey1 <392020814@qq.com>
2024-05-29 14:33:08 +0800

    fix bugs in figs

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def plot_clustering_points(X, result, save_name='DBSCAN'):
+def plot_clustering_points(X, result, save_name='DBSCAN.pdf'):
     X = np.array(X)
 
     fig = plt.figure(figsize=(10, 7))
@@ -13,5 +13,5 @@ def plot_clustering_points(X, result, save_name='DBSCAN'):
     ax1 = fig.add_subplot(122, projection='3d')
     ax1.scatter(X[:, 0], X[:, 1], X[:, 2], c=result["sklearn_cluster"].astype(int))
     ax1.set_title("Traditional DBSCAN")
-    plt.savefig(f"figs/{save_name}.pdf", dpi=300, bbox_inches='tight', pad_inches=0.1, transparent=True)
+    plt.savefig(f"figs/{save_name}", dpi=300, bbox_inches='tight', pad_inches=0.1, transparent=True)
     plt.show()
